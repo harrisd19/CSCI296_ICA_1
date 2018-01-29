8a38946ee92ed61dea11833aef3fff0a3422194d
Devon <stdsh07@moravian.edu>
2018-01-29 08:15:15 -0500

    Finished Class Activity

--- app/src/main/java/com/example/devon/myapp/MainActivity.java
+++ app/src/main/java/com/example/devon/myapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.devon.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.*;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void greet(View view) {
+
+        //get the resource reference for the EditText view
+        EditText txfName = (EditText) findViewById(R.id.txfName);
+        //get the input text from the EditText view object
+        String name = txfName.getText().toString();
+        //get reference to the TextView view
+        TextView txfGreet = (TextView) findViewById(R.id.txtfGreet);
+        //update the TextView text
+        txfName.setText(name);
+
     }
 }
